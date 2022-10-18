3ab6740a86ef9d65e0e2adf3ce353f0db86abf79
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-10-18 15:14:14 +0500

    fixed date import in posts

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,7 +1,7 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
-import Date from "../../components/Date";
+import Date from "../../components/date";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
